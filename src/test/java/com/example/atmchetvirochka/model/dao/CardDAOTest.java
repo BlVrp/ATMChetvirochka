9b91cb17019048a8800c58f25865ae78b26f22bf
Anton <blvrp@newtonideas.com>
2023-10-29 09:20:16 +0200

    finish CardDAO test coverage of methods used in project

--- src/test/java/com/example/atmchetvirochka/model/dao/CardDAOTest.java
+++ src/test/java/com/example/atmchetvirochka/model/dao/CardDAOTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.*;
 
 import javax.lang.model.type.NullType;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 
@@ -19,12 +20,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class CardDAOTest {
     private static CardDAO cardDAO;
     private static CardDTO cardDTO;
+
+    private static CardDTO receiverCardDTO;
     @BeforeAll
     static void setUpAll(){
         DatabaseInitializator.initializeDatabase(ConnectionConstantHolder.testConnectionUrl);
         cardDAO = new CardDAO(ConnectionConstantHolder.testConnectionUrl);
         cardDTO = new CardDTO("234", "233",
-                "fullName", 2202, 11, "2222", 222, 1, 2, 1);
+                "fullName", 2202, 11, "2222", 2220, 1, 2, 1);
+        receiverCardDTO = new CardDTO("235", "233",
+                "fullName", 2202, 11, "2222", 2220, 1, 2, 0);
     }
 
     @Test
@@ -49,6 +54,7 @@ class CardDAOTest {
                 counter++;
             }
             assertEquals(counter, 1);
+            connection.close();
 
     }
 
@@ -98,6 +104,7 @@ class CardDAOTest {
         assertEquals(responseInfo.data.account_id, cardDTO.account_id);
         assertEquals(responseInfo.data.attempts, cardDTO.attempts);
         assertEquals(responseInfo.data.is_default, cardDTO.is_default);
+        connection.close();
     }
 
     @Test
@@ -127,7 +134,7 @@ class CardDAOTest {
     @Test
     @Order(8)
     void makeTransferDoesNotWorkIfAmountExceedsBalance() {
-        TransactionDTO tooMuchMoneyTransaction = new TransactionDTO(cardDTO.card_number, null, 223);
+        TransactionDTO tooMuchMoneyTransaction = new TransactionDTO(cardDTO.card_number, null, 22222);
         ResponseInfo<NullType>responseInfo = cardDAO.makeTransfer(tooMuchMoneyTransaction);
         assertFalse(responseInfo.success);
         assertEquals(responseInfo.message, "Not enough money");
@@ -135,12 +142,65 @@ class CardDAOTest {
     @Test
     @Order(9)
     void makeTransferWorksAsWithdrawalIfTheReceiverIsNotSpecified() throws SQLException {
+        TransactionDTO withdrawTransaction = new TransactionDTO(cardDTO.card_number, null, 220);
+        ResponseInfo<NullType>responseInfo = cardDAO.makeTransfer(withdrawTransaction);
+        assertTrue(responseInfo.success);
         Connection connection = DriverManager.getConnection(ConnectionConstantHolder.testConnectionUrl);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM CARD");
-        TransactionDTO tooMuchMoneyTransaction = new TransactionDTO(cardDTO.card_number, null, 220);
-        ResponseInfo<NullType>responseInfo = cardDAO.makeTransfer(tooMuchMoneyTransaction);
-        assertTrue(responseInfo.success);
-
+        assertTrue(resultSet.next());
+        assertEquals(resultSet.getLong("balance"), cardDTO.balance-220);
+        ResultSet resultSet1 = statement.executeQuery("SELECT * FROM BANK_TRANSACTION");
+        assertTrue(resultSet1.next());
+        assertEquals(resultSet1.getString("sender"), cardDTO.card_number);
+        assertNull(resultSet1.getString("receiver"));
+        assertEquals(resultSet1.getLong("amount"), 220);
+        assertEquals(resultSet1.getString("transaction_date"), LocalDate.now().toString());
+        connection.close();
     }
+    @Test
+    @Order(10)
+    void makeTransferDoesNotWorkIfReceiverIsNotNullButCannotBeFound() throws SQLException {
+        TransactionDTO noReceiverTransaction = new TransactionDTO(cardDTO.card_number, "0000", 220);
+        ResponseInfo<NullType>responseInfo = cardDAO.makeTransfer(noReceiverTransaction);
+        assertFalse(responseInfo.success);
+        assertEquals(responseInfo.message, "Receiver does not exist");
+    }
+
+    @Test
+    @Order(11)
+    void makeTransferWorksIfReceiverIsSpecified() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(ConnectionConstantHolder.testConnectionUrl);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO CARD (card_number," +
+                    " cvv, full_name, due_to_year, due_to_month, account_id, pin, balance, attempts, is_default) " +
+                    "VALUES ('" + receiverCardDTO.card_number + "', '" + receiverCardDTO.cvv + "', '" + receiverCardDTO.full_name +
+                    "', " + receiverCardDTO.due_to_year + ", "
+                    + receiverCardDTO.due_to_month + ", " + receiverCardDTO.account_id + ", '" + receiverCardDTO.pin + "', "
+                    + receiverCardDTO.balance +
+                    ", " + receiverCardDTO.attempts + ", " + receiverCardDTO.is_default + ")");
+
+
+        }
+        TransactionDTO trueTransferTransaction = new TransactionDTO(cardDTO.card_number, receiverCardDTO.card_number, 220);
+        ResponseInfo<NullType>responseInfo = cardDAO.makeTransfer(trueTransferTransaction);
+        assertTrue(responseInfo.success);
+        try (Connection connection = DriverManager.getConnection(ConnectionConstantHolder.testConnectionUrl);
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM CARD WHERE card_number = 234");
+            assertTrue(resultSet.next());
+            assertEquals(resultSet.getLong("balance"), cardDTO.balance - 440);
+            ResultSet resultSet1 = statement.executeQuery("SELECT * FROM CARD WHERE card_number = 235");
+            assertTrue(resultSet1.next());
+            assertEquals(resultSet1.getLong("balance"), receiverCardDTO.balance + 220);
+            ResultSet resultSet2 = statement.executeQuery("SELECT * FROM BANK_TRANSACTION WHERE receiver = 235");
+            assertTrue(resultSet2.next());
+            assertEquals(resultSet2.getString("sender"), cardDTO.card_number);
+            assertEquals(resultSet2.getString("receiver"), receiverCardDTO.card_number);
+            assertEquals(resultSet2.getLong("amount"), 220);
+            assertEquals(resultSet2.getString("transaction_date"), LocalDate.now().toString());
+        }
+    }
+
+
 }
