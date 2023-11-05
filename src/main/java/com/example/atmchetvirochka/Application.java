a25048facdf8864f7fe049195ce311b792ae7301
Anton <blvrp@newtonideas.com>
2023-11-05 11:00:28 +0200

    reconfigure Application.java to display login menu screen by default

--- src/main/java/com/example/atmchetvirochka/Application.java
+++ src/main/java/com/example/atmchetvirochka/Application.java
@@ -20,9 +20,9 @@ import java.time.LocalDate;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("screens/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/example/atmchetvirochka/screens/loginmenu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 540);
+        stage.setTitle("ATM");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/atmchetvirochka/controller/LoginMenuController.java
@@ -0,0 +1,19 @@
+package com.example.atmchetvirochka.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class LoginMenuController {
+    @FXML
+    private Label textForUser;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Label incorrectPIN;
+
+    @FXML
+    private Label pin;
+}
